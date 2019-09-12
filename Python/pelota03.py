3fc1cd5d9f27b7244277c931c0ada0690c33cc63
317295869 <eduardovale09@ciencias.unam.mx>
2019-09-12 13:42:04 -0500

    0

---
+++ Python/pelota03.py
@@ -0,0 +1,15 @@
+#
+# Eduardo Alexis Valencia Dorantes
+# python 3.5.3
+# 12/09/2019
+#
+# Calcula la posición de una pelota con una velocidad inicial
+# de 5 m/s y un tiempo de 0.6 segundos utilizando nombres de variables
+# descriptivos pero muuuuuy largos
+
+velocidad_inicial = 5
+constante_de_gravedad = 9.81
+TIEMPO = 0.6
+PosicionVerticalDeLaBola = velocidad_inicial*TIEMPO -\
+                           0.5*constante_de_gravedad*TIEMPO**2
+print(PosicionVerticalDelaBola)
