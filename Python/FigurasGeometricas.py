0143e44e364f5201db5c78b743011230236a3aa8
317295869 <eduardovale09@ciencias.unam.mx>
2019-09-17 14:55:11 -0500

    FigurasGeometricas

---
+++ Python/FigurasGeometricas.py
@@ -0,0 +1,31 @@
+#
+# Eduardo Alexis Valencia Dorantes
+# python 3.5.3
+# 17/09/2019
+#
+#
+# Triangulo equilatero de lado 2
+# 2 con un vertice en el
+# origen en el cuadrante I
+# y un lado sobre el eje
+# de las x
+
+xA = 0
+yA = 0
+xB = 2
+yB = 0
+xC = 1
+yC = 2
+
+A = (xA,yA)
+B = (xB,yB)
+C = (xC,yC)
+
+print('''Las coordenadas del triangulo son''')
+print(A)
+print(B)
+print(C)
+
+
+
+
