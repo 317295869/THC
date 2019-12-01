511702ba4d15c5369fe5437979749719fd1df27e
Eduardo Alexis Valencia Dorantes <eduardovale09@ciencias.unam.mx>
2019-12-01 15:16:44 -0600

    Se agrego BuscarErrores.py a OK-Python

---
+++ OK/OK-Python/BuscarErrores.py
@@ -0,0 +1,38 @@
+from math import tan
+a = 2
+print(a)
+a1 = 'b'
+print(a1)
+x = 2
+y = x + 4 # y vale 6?
+print(y)
+pi = 3.14159
+print (tan (pi))
+c = 4**3**2**3
+_ = ((c-78564)/c + 32)
+print(_)
+descuento = '12%'
+print(descuento)
+MONTO = '120.-'
+print(MONTO)
+monto = '$120'
+print(monto)
+direccion = '<sucorreo>@ciencias.unam.mx>'
+print(direccion)
+# and = duck
+# class = "INF1100, gr 2"
+continue_ = x > 0
+print(continue_)
+rev = fox = True
+Noruego = ['un lenguaje humano']
+true = fox is rev in Noruego
+print(true)
+
+"""
+Sugerencias.
+
+Verificar por separado el lado izquierdo y derecho de la igualdad.
+Verificar la validez de los nombres de las variables
+Las utimas tres lineas funcionan correctamente, explique que es lo
+que hacen.
+"""
